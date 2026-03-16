eb116d3a5c3dd4f81f8ced86ffcd3806a7e2a9fd
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-16 15:30:38 +0530

    navbar structure create

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div>
+            
+        </div>
+
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
