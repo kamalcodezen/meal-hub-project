75d28e1165cac510df8cee4aef5184d8d2723502
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-16 16:04:23 +0530

    navbar complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import navImg from "../../assets/logo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-      <nav>
-        <div>
-            
+      <nav className="nav-container">
+        <div className="logo">
+          <img src={navImg} alt="window picture" />
+          <h2>Meal Hub</h2>
         </div>
 
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+        <ul className="menu">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Feature</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
         </ul>
       </nav>
     </div>
